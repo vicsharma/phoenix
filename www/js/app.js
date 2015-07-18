093562ba245947f1eb4d94d2877059829cee93f7
hnsreddy <hnsreddy@gmail.com>
2015-07-18 19:23:05 +0530

    Cleaning up...
    Cleaning up code that is not required.

--- www/js/app.js
+++ www/js/app.js
@@ -48,25 +48,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'veg.items'])
         ,controller : 'vegItemsController'
       }
     }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/veg');
