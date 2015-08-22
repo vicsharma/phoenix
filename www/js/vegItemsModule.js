51308dd978cb1872d31ae39cc62845d7099c64b1
hnsreddy <hnsreddy@gmail.com>
2015-08-22 18:37:46 +0530

    Update side description with selected item
    Update side description with selected item, currently only the
    description of the item selected with the image is displayed.

--- www/js/vegItemsModule.js
+++ www/js/vegItemsModule.js
@@ -2,5 +2,14 @@ var vegItems = angular.module('veg.items', ['vegMenuService']);
 vegItems.controller('vegItemsController', function($scope, getMenuItems) {
 	getMenuItems().then(function(response) {
 		$scope.vegItems = response.data;
+		$scope.itemsMap = [];
+		for (item in $scope.vegItems) {
+			var currentItem = $scope.vegItems[item];
+			$scope.itemsMap[currentItem.id] = currentItem;
+		}
 	});
+
+	$scope.showMoreInfo = function (itemId) {
+		$scope.chosenItem = $scope.itemsMap[itemId]
+	}
 })
