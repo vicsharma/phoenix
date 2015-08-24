83ffdd338f04af3bd3a979a9e90679f8fdfbf533
Nikhil <nikhil.sharma.ece@gmail.com>
2015-08-24 10:29:37 +0530

    Navigation to order summary from cart
    Don’t be surprise to see 3 cart icons it is basically 3 different ways
    to navigate let decide which one is best.
    Also added few more items to json.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout,$state) {
   $scope.$root.cartIcon = "ion-ios-cart-outline";
+
+  $scope.navigate=function(){
+  	console.log($state);
+  	$state.go('app.order-summary');
+  };
+  
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
