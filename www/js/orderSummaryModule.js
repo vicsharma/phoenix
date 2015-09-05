be1482c73586d7f3eaf50bacdf2da4b3748f2352
Vivek <vsdutt@gmail.com>
2015-09-05 18:47:26 +0530

    Add and subtract quantity
    In order summary page add and subtract quantity

--- www/js/orderSummaryModule.js
+++ www/js/orderSummaryModule.js
@@ -2,4 +2,10 @@ var orderSummary = angular.module('veg.orderSummary', ['orderService']);
 orderSummary.controller('orderSummaryController', function($scope, orderDetails) {
   console.log(orderDetails);
   $scope.orderDetails = orderDetails;
+  $scope.addItem = function (order) {
+    order.quantity = order.quantity + 1;
+  }
+  $scope.deleteItem = function (order) {
+    order.quantity = order.quantity - 1;
+  }
 });
