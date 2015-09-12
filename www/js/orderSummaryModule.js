60008cd9dad9ec1cebafdef953a4c074ca2283fd
hnsreddy <hnsreddy@gmail.com>
2015-09-12 12:57:50 +0530

    Message when nothing in the cart
    Message when nothing in the cart.

--- www/js/orderSummaryModule.js
+++ www/js/orderSummaryModule.js
@@ -2,22 +2,25 @@ var orderSummary = angular.module('veg.orderSummary', ['orderService']);
 
 orderSummary.controller('orderSummaryController', function($scope, $state, OrderService) {
 
-  $scope.orderDetails = OrderService.getOrderDetails();
+    $scope.orderDetails = OrderService.getOrderDetails();
 
-  $scope.addItem = function (item) {
-    OrderService.addItemToOrder(item);
-  }
-
-  $scope.deleteItem = function (item) {
-    OrderService.deleteItemFromOrder(item);
-    if (OrderService.isOrderEmpty()) {
-      $scope.$root.cartIcon = "ion-ios-cart-outline";
-      $state.go('app.veg-items');
+    $scope.addItem = function (item) {
+        OrderService.addItemToOrder(item);
     }
-  }
 
-  $scope.getFinalPrice=function(item){
-    return item.price * item.quantity;
-  }
+    $scope.orderEmpty = function () {
+        return OrderService.isOrderEmpty();
+    }
 
+    $scope.deleteItem = function (item) {
+        OrderService.deleteItemFromOrder(item);
+        if (OrderService.isOrderEmpty()) {
+            $scope.$root.cartIcon = "ion-ios-cart-outline";
+            $state.go('app.veg-items');
+        }
+    }
+
+    $scope.getFinalPrice=function(item){
+        return item.price * item.quantity;
+    }
 });
