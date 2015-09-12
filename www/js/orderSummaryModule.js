9774db77c821efa806d8ab2fa1da9711ee21a568
hnsreddy <hnsreddy@gmail.com>
2015-09-12 11:28:06 +0530

    Minor tweaks
    Navigate to menu automagically when no items in the order.

--- www/js/orderSummaryModule.js
+++ www/js/orderSummaryModule.js
@@ -1,13 +1,20 @@
 var orderSummary = angular.module('veg.orderSummary', ['orderService']);
-orderSummary.controller('orderSummaryController', function($scope, orderDetails) {
+orderSummary.controller('orderSummaryController', function($scope, $state, orderDetails) {
   $scope.orderDetails = orderDetails;
+
   $scope.addItem = function (order) {
     order.quantity = order.quantity + 1;
   }
+
   $scope.deleteItem = function (order) {
   	if(order.quantity <= 1 ){
   		delete orderDetails[order.id];
+        if(Object.keys(orderDetails).length == 0) {
+          $scope.$root.cartIcon = "ion-ios-cart-outline";
+          $state.go('app.veg-items');
+        }
   	}
     order.quantity = order.quantity - 1;
   }
+
 });
